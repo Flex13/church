1540188d564d8084539b16d3de34698a0d04d8b5
Sibanye <sibanyebukani01@gmail.com>
2020-07-26 05:00:33 +0200

    Church UI DEV

---
+++ about.php
@@ -0,0 +1,68 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/nav.php'); ?>
+
+<header>
+    <!-- Background image -->
+    <div id="about" class="bg-image shadow-2-strong">
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
+            <div class="container d-flex align-items-center justify-content-center text-center h-100">
+                <div class="text-white">
+                    <h1>About</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Background image -->
+</header>
+
+
+
+
+<main class="my-5 sidebar-layout">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!--Section: Content-->
+
+                <section class="my-5">
+                    <h3 class="mb-5"><strong>VISION</strong></h3>
+
+                    <p class="text-muted">We exist to bring men and women to live the tangible reality of the promises that God made in His Word, the Bible. His promises can be experienced in the individual life, family, city or in the nation.</p>
+
+                    <h3 class="mb-5"><strong>MISSION</strong></h3>
+
+                    <p class="text-muted">
+                        Our mission is to make known the promises of God made for all humanity, and help them to live in reality of them. At the center of our mission is Jesus Christ who fulfilled all God’s promises (2 Corinthians 1:20).
+                    </p>
+
+                    <p class="text-muted">This will be done in obedience to the command of our Lord Jesus Christ: “Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, and TEACHING them to obey EVERYTHING I have commanded you” (Matthew 28:19-20a)
+                    <br><br>We are committed to our Church mission and will live true to it by:</p>
+
+                    <p class="text-muted">
+                    • Thoroughly teaching the realities of God’s promises as in God’s word<br>
+                    • Developing to be able to perpetuity this task to the next generation till Christ appears.<br>
+                    • Making know all these realities to all people, families, societies or nations.<br>
+                    </p>
+
+
+
+
+                </section>
+                <!--Section: Content-->
+            </div>
+
+            <div class="col-md-4">
+
+                <?php include('includes/sidebar.php'); ?>
+
+            </div>
+        </div>
+
+    </div>
+</main>
+<!--Main layout-->
+
+
+
+
+<?php include('includes/footer.php'); ?>
