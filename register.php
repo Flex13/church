2badaf6c075f6003c83fca26c9da8a30796a1d4f
Sibanye <sibanyebukani01@gmail.com>
2020-08-06 18:54:55 +0200

    chamges

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/nav.php'); ?>
 
-<<<<<<< HEAD
 <!--Main layout-->
 <div id="register" class="bg-image shadow-2-strong">
         <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
@@ -72,88 +71,3 @@
 
 
 <?php include('includes/footer.php'); ?>
-=======
-<section>
-    <!-- Background image -->
-    <div id="register" class="bg-image">
-        <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
-            <div class="container d-flex align-items-center h-100">
-                <div class="row d-flex justify-content-center">
-                    <div class="col-xl-5 col-md-8">
-                        <form class="bg-white rounded shadow-5-strong p-5">
-                            <!-- Email input -->
-                            <div class="form-outline mb-4">
-                                <input type="text" name="name" id="name" class="form-control" />
-                                <label class="form-label" for="name">First Name</label>
-                            </div>
-
-                            <!-- Email input -->
-                            <div class="form-outline mb-4">
-                                <input type="Surname" name="surname" id="surname" class="form-control" />
-                                <label class="form-label" for="surname">Surname</label>
-                            </div>
-                            <!-- Email input -->
-                            <div class="form-outline mb-4">
-                                <input type="email" name="email" id="email" class="form-control" />
-                                <label class="form-label" for="email">Email address</label>
-                            </div>
-
-                            <!-- Password input -->
-                            <div class="form-outline mb-4">
-                                <input type="password" name="password" id="password" class="form-control" />
-                                <label class="form-label" for="password">Password</label>
-                            </div>
-
-                            <!-- Submit button -->
-                            <button type="button" id="submit" class="btn btn-primary btn-block">Register</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Background image -->
-</section>
-
-
-<section>
-    <!-- Background image -->
-    <div id="register2" class="bg-image">
-        <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
-            <div class="container d-flex align-items-center h-100">
-                <div class="row d-flex justify-content-center">
-                    <div class="text-white">
-                        <!--Grid row-->
-                        <div class="row text-center">
-                            <!--Grid column-->
-                            <div class="col-lg-12 col-md-12 mb-4 mb-md-0">
-                                <h4 class="text-uppercase">PFC Ministries</h4>
-                                <h1><i class="fab fa-facebook" style="color: #3b5998;"></i> | <i class="fab fa-twitter" style="color: #1da1f2;"></i> | <i class="fab fa-instagram" style="color: #c32aa3;"></i> | <i class="fab fa-youtube" style="color: #ff0000;"></i></h1>
-                                <p>Contact : Africa Office at info@pfcministries.co.za | Tel: +27 61 382 0059</p>
-                                <p>Opening Hours: Mon - Fri: 8am-8pm,​​ Saturday: 9am-7pm, ​Sunday: 9am-8pm</p>
-                            </div>
-                            <!--Grid column-->
-                        </div>
-                        <!--Grid row-->
-                        <!-- Copyright -->
-                        <div class="text-center p-0">
-                            <small>2020 © <a class="text-primary" href="https://digitalanalytica.co.za/">Digital&Analytica</a></small>
-                        </div>
-                        <!-- Copyright -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Background image -->
-</section>
-
-</body>
-
-<!-- MDB -->
-<script type="text/javascript" src="js/mdb.min.js"></script>
-<!-- Custom scripts -->
-<script type="text/javascript"></script>
-
-</html>
->>>>>>> 66ae2a66add9a72a8103aa0ac4a0b65fa6b2b356
